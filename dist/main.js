fba004f3440395966bcdd7ebbed0f0762531e108
ahmed <elshehry@gamil.com>
2023-08-12 00:51:08 +0300

    The complete logic

--- dist/main.js
+++ dist/main.js
@@ -8,11 +8,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-function getWeather(city = `cairo`) {
+const dateParag = Array.from(document.querySelectorAll(".box header p"));
+const cityParag = Array.from(document.querySelectorAll("#city"));
+const tempParag = Array.from(document.querySelectorAll("#temp"));
+const conditionParag = Array.from(document.querySelectorAll("#condition"));
+const rainSpans = Array.from(document.querySelectorAll("#rain"));
+const windSpans = Array.from(document.querySelectorAll("#wind"));
+const locationSearch = document.getElementById("location-search");
+const btn = document.getElementById("btn");
+var city = "cairo";
+function setDateHeader(dateParag) {
+    for (let i = 0; i < 6; i += 2) {
+        let today = new Date();
+        let nextDay = new Date();
+        nextDay.setDate(today.getDate() + i / 2);
+        nextDay = nextDay.toDateString().slice(0, 10).split(" ");
+        dateParag[i].innerHTML = nextDay[0];
+        dateParag[i + 1].innerHTML = `${nextDay[2]} ${nextDay[1]}`;
+    }
+}
+function setLocation(country, gov) {
+    cityParag.map(e => e.innerHTML = `${country}-${gov}`);
+}
+function setWeather(forCastDay) {
+    tempParag.map((e, i) => e.innerHTML = `${forCastDay[i].day.avgtemp_c}`);
+    conditionParag.map((e, i) => e.innerHTML = forCastDay[i].day.condition.text);
+    rainSpans.map((e, i) => e.innerHTML = forCastDay[i].day.daily_chance_of_rain);
+    windSpans.map((e, i) => e.innerHTML = forCastDay[i].day.maxwind_kph);
+}
+function getWeather(city) {
     return __awaiter(this, void 0, void 0, function* () {
         const resp = yield fetch(`https://api.weatherapi.com/v1/forecast.json?key=e34cd4dd46884d30983114130230708&q=${city}&days=3`);
-        let data = (yield resp.json()).forecast.forecastday;
-        console.log(data);
+        let data = (yield resp.json());
+        let country = data.location.country;
+        let gov = data.location.name;
+        let forCastDay = data.forecast.forecastday;
+        setLocation(country, gov);
+        setWeather(forCastDay);
+        setDateHeader(dateParag);
     });
 }
-getWeather();
+getWeather(city);
+btn.onclick = () => {
+    city = locationSearch.value;
+    getWeather(city);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,51 @@
-async function getWeather(city = `cairo`) {
-    const resp = await fetch(
-        `https://api.weatherapi.com/v1/forecast.json?key=e34cd4dd46884d30983114130230708&q=${city}&days=3`
-    );
-    let data = (await resp.json()).forecast.forecastday
-    console.log(data);
+const dateParag: HTMLParagraphElement[] = Array.from(document.querySelectorAll(".box header p"));
+const cityParag: HTMLParagraphElement[] = Array.from(document.querySelectorAll("#city"))
+const tempParag: HTMLParagraphElement[] = Array.from(document.querySelectorAll("#temp"));
+const conditionParag: HTMLParagraphElement[] = Array.from(document.querySelectorAll("#condition"));
+const rainSpans: HTMLSpanElement[] = Array.from(document.querySelectorAll("#rain"));
+const windSpans: HTMLSpanElement[] = Array.from(document.querySelectorAll("#wind"));
+const locationSearch = <HTMLInputElement>document.getElementById("location-search");
+const btn = <HTMLButtonElement>document.getElementById("btn");
+var city: string = "cairo";
 
+function setDateHeader(dateParag: HTMLParagraphElement[]) {
+    for (let i = 0; i < 6; i += 2) {
+        let today = new Date();
+        let nextDay: any = new Date();
+        nextDay.setDate(today.getDate() + i / 2);
+        nextDay = nextDay.toDateString().slice(0, 10).split(" ");
+        dateParag[i].innerHTML = nextDay[0]
+        dateParag[i + 1].innerHTML = `${nextDay[2]} ${nextDay[1]}`;
+    }
+}
+
+function setLocation(country: string, gov: string) {
+    cityParag.map(e => e.innerHTML = `${country}-${gov}`)
 
 }
-getWeather()
+
+function setWeather(forCastDay: any) {
+    tempParag.map((e, i) => e.innerHTML = `${forCastDay[i].day.avgtemp_c}`);
+    conditionParag.map((e, i) => e.innerHTML = forCastDay[i].day.condition.text);
+    rainSpans.map((e, i) => e.innerHTML = forCastDay[i].day.daily_chance_of_rain);
+    windSpans.map((e, i) => e.innerHTML=forCastDay[i].day.maxwind_kph);
+}
+
+async function getWeather(city: string) {
+    const resp = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=e34cd4dd46884d30983114130230708&q=${city}&days=3`);
+    let data: any = (await resp.json());
+    let country: string = data.location.country;
+    let gov: string = data.location.name;
+    let forCastDay = data.forecast.forecastday;
+    setLocation(country, gov);
+    setWeather(forCastDay);
+    setDateHeader(dateParag);
+}
+getWeather(city);
+
+
+btn.onclick = () => {
+    city = locationSearch.value;
+    getWeather(city);
+}
+
